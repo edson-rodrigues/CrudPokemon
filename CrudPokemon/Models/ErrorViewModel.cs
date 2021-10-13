7c4b7e9a654bcc535b2193b1b71aaadca6a11d98
Edson Rodrigues <edson.rodgon@gmail.com>
2021-10-13 08:44:25 -0300

    initial commit

---
+++ CrudPokemon/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CrudPokemon.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
